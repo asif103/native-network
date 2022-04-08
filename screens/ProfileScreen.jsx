94d06c668c32adeb7942f844d9a59370def10aa4
Mohammed Asif <asif.cse473@gmail.com>
2022-04-09 00:15:24 +0600

    profile screen and tweet detail screen designed

--- screens/ProfileScreen.jsx
+++ screens/ProfileScreen.jsx
@@ -1,19 +1,214 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+    Dimensions,
+    Image,
+    StyleSheet,
+    TouchableOpacity,
+    View,
+    Text,
+    TouchableOpacityComponent,
+    Linking, FlatList
+} from "react-native";
+import {EvilIcons} from '@expo/vector-icons';
+import {AntDesign} from '@expo/vector-icons';    const DATA = [
+    {
+        id: 1,
+        title: "title one"
+
+    },
+    {
+        id: 2,
+        title: "title two"
+
+    },
+    {
+        id: 3,
+        title: "title Three"
+
+    },
+    {
+        id: 4,
+        title: "title four"
+
+    },
+    {
+        id: 5,
+        title: "title five"
+
+    },
+    {
+        id: 6,
+        title: "title six"
+
+    },
+    {
+        id: 7,
+        title: "title seven"
+
+    },
+    {
+        id: 8,
+        title: "title eight"
+
+    },
+    {
+        id: 9,
+        title: "title nine"
+
+    },
+    {
+        id: 10,
+        title: "title ten"
+
+    }
+];
+
+
+
 
 const ProfileScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text>profile</Text>
-    </View>
-  );
+    const {width, height} = Dimensions.get('window')
+    const profileHeader =()=> (
+            <View style={styles.container}>
+                <Image
+                    source={{uri: 'https://snack-web-player.s3.us-west-1.amazonaws.com/v2/43/static/media/react-native-logo.79778b9e.png'}}
+                    style={styles.coverImage}/>
+                <View style={styles.profileImageSection}>
+                    <Image
+                        source={{uri: 'https://snack-web-player.s3.us-west-1.amazonaws.com/v2/43/static/media/react-native-logo.79778b9e.png'}}
+                        style={styles.avatar}/>
+                    <TouchableOpacity style={styles.followButton}>
+                        <Text style={styles.followButtonText}>Follow</Text>
+                    </TouchableOpacity>
+                </View>
+                <View>
+                    <Text numberOfLines={1} style={styles.tweetName}>User Name</Text>
+                    <Text numberOfLines={1} style={styles.tweetHandle}>@username</Text>
+                </View>
+                <View>
+                    <Text style={styles.aboutContent}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab
+                        cupiditate impedit inventore laborum vero? Dolores fugiat harum nostrum odio reiciendis.</Text>
+                </View>
+                <View style={[styles.infoContainer, styles.flexRow]}>
+                    <EvilIcons name="location" size={24} color="black"/>
+                    <Text style={[styles.textGray, styles.ml4]}>Dhaka, Bangladesh</Text>
+                </View>
+                <View style={[styles.infoContainer, styles.flexRow]}>
+                    <TouchableOpacity onPress={() => Linking.openURL('https://wordpress.org')}>
+                        <View style={styles.flexRow}>
+                            <EvilIcons name="link" size={24} color="gray"/>
+                            <Text style={{color: "blue"}}>website.com</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <View style={[styles.flexRow, styles.ml4]}>
+                        <AntDesign name="calendar" size={20} color="gray"/>
+                        <Text style={[styles.textGray, styles.ml4]}>Dhaka, Bangladesh</Text>
+                    </View>
+                </View>
+                <View style={[styles.infoContainer, styles.flexRow]}>
+                    <View style={styles.flexRow}>
+                        <Text style={styles.engagementCount}>1,250</Text>
+                        <Text style={styles.engagementText}>Following</Text>
+                    </View>
+                    <View style={styles.flexRow}>
+                        <Text style={styles.engagementCount}>1,250</Text>
+                        <Text style={styles.engagementText}>Followers</Text>
+                    </View>
+                </View>
+                <View style={styles.ItemSeparator}/>
+            </View>
+        );
+
+    const renderItem = ({ item }) => (
+        <View style={{marginVertical:10 }}>
+        <Text>{item.title} </Text>
+        </View>
+    );
+    return (
+        <FlatList
+            data={DATA}
+            renderItem={renderItem}
+            keyExtractor={item => item.id}
+            ListHeaderComponent={profileHeader}
+        />
+    );
 };
 export default ProfileScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+    },
+    coverImage: {
+        width: Dimensions.get('window').width,
+        height: 150
+    },
+    avatar: {
+        width: 80,
+        height: 80,
+        borderRadius: 40,
+        borderColor: "#ffffff",
+        borderWidth: 4
+    },
+    followButton: {
+        height: 40,
+        backgroundColor: "#222222",
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 20
+    },
+    followButtonText: {
+        color: "#ffffff",
+        paddingHorizontal: 20,
+        fontWeight: "bold"
+    },
+    profileImageSection: {
+        flexDirection: "row",
+        alignItems: "flex-end",
+        justifyContent: "space-between",
+        paddingHorizontal: 12,
+        marginTop: -30
+    },
+    tweetName: {
+        fontSize: 24,
+        fontWeight: "700",
+        paddingHorizontal: 12,
+    },
+    tweetHandle: {
+        marginHorizontal: 8,
+        color: "gray",
+        paddingHorizontal: 12,
+    },
+    aboutContent: {
+        paddingHorizontal: 12,
+        paddingVertical: 4
+    },
+    textGray: {
+        color: "gray"
+    },
+    ml4: {
+        marginLeft: 4
+    },
+    infoContainer: {
+        paddingHorizontal: 12,
+        paddingVertical: 6,
+    },
+    flexRow: {
+        flexDirection: "row",
+    },
+    engagementCount: {
+        fontWeight: "bold",
+        paddingRight: 4
+    },
+    engagementText: {
+        color: "gray",
+        paddingRight: 10
+    },
+    ItemSeparator: {
+        backgroundColor: "#ddd",
+        height: 1,
+        width: "100%"
+
+    },
+
 });
--- screens/TweetScreen.jsx
+++ screens/TweetScreen.jsx
@@ -1,19 +1,132 @@
-import { StyleSheet, Text, View } from "react-native";
+import {Image, StyleSheet, Text, View, TouchableOpacity, Platform} from "react-native";
+import {Entypo, EvilIcons} from '@expo/vector-icons';
 
-const TweetScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Tweet</Text>
-    </View>
-  );
+const TweetScreen = ({navigation}) => {
+    const goToProfile = () => {
+        navigation.navigate('Profile Screen')
+    }
+    return (
+        <View style={styles.container}>
+            <View style={styles.profileContainer}>
+                <TouchableOpacity style={styles.flexRow} onPress={goToProfile}>
+                    <Image
+                        source={{uri: 'https://snack-web-player.s3.us-west-1.amazonaws.com/v2/43/static/media/react-native-logo.79778b9e.png'}}
+                        style={styles.avatar}/>
+                    <View>
+                        <Text numberOfLines={1} style={styles.tweetName}>User Name</Text>
+                        <Text numberOfLines={1} style={styles.tweetHandle}>@username</Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Entypo name="dots-three-vertical" size={20} color="gray"/>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.tweetContentContainer}>
+                <Text style={styles.tweetContent}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet aspernatur ipsam itaque iusto non,
+                    qui quo totam veritatis voluptatum. Aliquam beatae corporis cumque doloremque dolorum, error illum
+                    inventore, ipsum itaque, laudantium libero minus molestiae quia quibusdam quis suscipit
+                    voluptatibus? Dicta eaque, earum ipsam laboriosam laborum libero modi, nihil nostrum odio odit omnis
+                    quod rem, reprehenderit repudiandae sint tempora tenetur. Ab adipisci alias, aliquid amet architecto
+                    aut autem commodi consectetur consequatur cumque dolore eligendi ex excepturi facilis harum hic illo
+                    ipsum, iure non nostrum numquam provident quasi ratione sed sit tempora ullam ut vel voluptatem
+                    voluptatibus. Ab consequuntur quis reiciendis?
+                </Text>
+            </View>
+            <View style={styles.ItemSeparator}/>
+            <View style={[styles.tweetEngagement, styles.flexRow]}>
+                <View style={styles.flexRow}>
+                    <Text style={styles.engagementCount}>1,250</Text>
+                    <Text style={styles.engagementText}>Retweets</Text>
+                </View>
+                <View style={styles.flexRow}>
+                    <Text style={styles.engagementCount}>1,250</Text>
+                    <Text style={styles.engagementText}>Quote Tweets</Text>
+                </View>
+                <View style={styles.flexRow}>
+                    <Text style={styles.engagementCount}>1,250</Text>
+                    <Text style={styles.engagementText}>Likes</Text>
+                </View>
+            </View>
+            <View style={styles.ItemSeparator}/>
+            <View style={styles.tweetEngagementIconsContainer}>
+                <TouchableOpacity style={styles.flexRow}>
+                    <EvilIcons name="comment" size={26} color="gray" />
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles.flexRow, styles.ml4]}>
+                    <EvilIcons name="retweet" size={26} color="gray" />
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles.flexRow, styles.ml4]}>
+                    <EvilIcons name="heart" size={26} color="gray" />
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles.flexRow, styles.ml4]}>
+                    <EvilIcons name={Platform.OS === 'ios'?'share-apple':'share-google'} size={26} color="gray" />
+                </TouchableOpacity>
+            </View>
+            <View style={styles.ItemSeparator}/>
+        </View>
+    );
 };
 export default TweetScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff"
+    },
+    flexRow: {
+        flexDirection: "row",
+
+    },
+    profileContainer: {
+        paddingVertical: 12,
+        paddingHorizontal: 12,
+        flexDirection: "row",
+        justifyContent: "space-between"
+    },
+    avatar: {
+        width: 50,
+        height: 50,
+        marginRight: 12,
+        borderRadius: 25
+    },
+    tweetName: {
+        fontWeight: "bold",
+        color: "#222222"
+    },
+    tweetHandle: {
+        color: "gray"
+    },
+    tweetContentContainer: {
+        paddingVertical: 12,
+        paddingHorizontal: 12
+    },
+    tweetContent: {
+        fontSize: 16,
+        lineHeight: 24
+    },
+    ItemSeparator: {
+        backgroundColor: "#ddd",
+        height: 1,
+        width: "100%"
+
+    },
+    tweetEngagement:{
+        paddingHorizontal:12,
+        paddingVertical: 12,
+    },
+    engagementCount:{
+        fontWeight:"bold",
+        paddingRight:4
+    },
+    engagementText:{
+        color:"gray",
+        paddingRight:10
+    },
+    tweetEngagementIconsContainer:{
+        paddingVertical: 12,
+        paddingHorizontal: 12,
+        flexDirection:"row",
+        justifyContent:"space-between"
+    }
 });
