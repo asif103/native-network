02fb56a7e101d8482de2d2b18bcff3472e33c6d7
Mohammed Asif <asif.cse473@gmail.com>
2022-04-16 01:36:11 +0600

    infinite scroll added

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,7 +1,7 @@
 import {ActivityIndicator, FlatList, Platform, StyleSheet, Text, TouchableOpacityComponent, View} from "react-native";
 import {Image, TouchableOpacity} from "react-native";
-import { EvilIcons } from '@expo/vector-icons';
-import { AntDesign } from '@expo/vector-icons';
+import {EvilIcons} from '@expo/vector-icons';
+import {AntDesign} from '@expo/vector-icons';
 import {useEffect, useState} from "react";
 import axios from "axios";
 import {formatDistanceToNowStrict} from "date-fns";
@@ -9,17 +9,23 @@ import locale from 'date-fns/locale/en-US'
 import formatDistance from "../helpers/formatDistanceCustom";
 
 
-
-
 const HomeScreen = ({navigation}) => {
     const [isLoading, setIsLoading] = useState(true)
     const [tweet, setTweet] = useState([])
     const [isRefreshing, setIsRefreshing] = useState(false)
-    const fetchAllTweets =() =>{
-        axios.get(`http://78a2-103-210-58-23.ngrok.io/api/tweets`)
-            .then(response =>{
-
-                setTweet(response.data)
+    const [page, setPage] = useState(1)
+    const [isAtEndOfScrolling, setIsAtEndOfScrolling] = useState(false)
+    const fetchAllTweets = () => {
+        axios.get(`http://78a2-103-210-58-23.ngrok.io/api/tweets/?page=${page}`)
+            .then(response => {
+                if (page === 1) {
+                    setTweet(response.data.data)
+                } else {
+                    setTweet([...tweet, ...response.data.data])
+                }
+                if(!response.data.next_page_url) {
+                    setIsAtEndOfScrolling(true)
+                }
                 setIsLoading(false)
                 setIsRefreshing(false)
             })
@@ -29,13 +35,18 @@ const HomeScreen = ({navigation}) => {
                 setIsRefreshing(false)
             })
     }
-    const handleRefresh = () =>{
+    const handleRefresh = () => {
         fetchAllTweets()
         setIsRefreshing(true)
+        setPage(1)
+        setIsAtEndOfScrolling(false)
     }
-    useEffect(()=>{
+    const handleEnd = () => {
+        setPage(page + 1)
+    }
+    useEffect(() => {
         fetchAllTweets()
-    },[])
+    }, [page])
     const goToProfile = () => {
         navigation.navigate('Profile Screen')
     }
@@ -45,7 +56,7 @@ const HomeScreen = ({navigation}) => {
     const goToNewTweet = () => {
         navigation.navigate("New Tweet")
     }
-    const TweetItem = ({item:tweet}) => (
+    const TweetItem = ({item: tweet}) => (
         <View style={styles.tweetContainer}>
             <TouchableOpacity onPress={goToProfile}>
 
@@ -74,19 +85,20 @@ const HomeScreen = ({navigation}) => {
                 </TouchableOpacity>
                 <View style={styles.tweetEngagement}>
                     <TouchableOpacity style={styles.flexRow}>
-                        <EvilIcons name="comment" size={22} color="gray" />
+                        <EvilIcons name="comment" size={22} color="gray"/>
                         <Text style={styles.textGray}>150</Text>
                     </TouchableOpacity>
                     <TouchableOpacity style={[styles.flexRow, styles.ml4]}>
-                        <EvilIcons name="retweet" size={22} color="gray" />
+                        <EvilIcons name="retweet" size={22} color="gray"/>
                         <Text style={styles.textGray}>10</Text>
                     </TouchableOpacity>
                     <TouchableOpacity style={[styles.flexRow, styles.ml4]}>
-                        <EvilIcons name="heart" size={22} color="gray" />
+                        <EvilIcons name="heart" size={22} color="gray"/>
                         <Text style={styles.textGray}>10,450</Text>
                     </TouchableOpacity>
                     <TouchableOpacity style={[styles.flexRow, styles.ml4]}>
-                        <EvilIcons name={Platform.OS === 'ios'?'share-apple':'share-google'} size={22} color="gray" />
+                        <EvilIcons name={Platform.OS === 'ios' ? 'share-apple' : 'share-google'} size={22}
+                                   color="gray"/>
                     </TouchableOpacity>
                 </View>
             </View>
@@ -96,22 +108,25 @@ const HomeScreen = ({navigation}) => {
         <View style={styles.container}>
             {
                 isLoading &&
-                <ActivityIndicator style={{marginTop:8}} size="large" color="gray" />
+                <ActivityIndicator style={{marginTop: 8}} size="large" color="gray"/>
             }
             <FlatList
                 data={tweet}
                 renderItem={TweetItem}
                 keyExtractor={item => item.id.toString()}
-                ItemSeparatorComponent={()=><View style={styles.ItemSeparator}/>}
+                ItemSeparatorComponent={() => <View style={styles.ItemSeparator}/>}
                 refreshing={isRefreshing}
                 onRefresh={handleRefresh}
+                onEndReached={handleEnd}
+                onEndReachedThreshold={0}
+                ListFooterComponent={() => !isAtEndOfScrolling && (<ActivityIndicator style={{marginTop: 8}} size="large" color="gray"/>)}
             />
             {/*<Button*/}
             {/*  title={"create new tweet"}*/}
             {/*  onPress={() => navigation.navigate("New Tweet")}*/}
             {/*/>*/}
             <TouchableOpacity style={styles.floatingButton} onPress={goToNewTweet}>
-                <AntDesign name="pluscircle" size={50} color="#1d9bf1" />
+                <AntDesign name="pluscircle" size={50} color="#1d9bf1"/>
             </TouchableOpacity>
         </View>
     );
@@ -160,19 +175,19 @@ const styles = StyleSheet.create({
     textGray: {
         color: "gray"
     },
-    ml4:{
-        marginLeft:4
+    ml4: {
+        marginLeft: 4
     },
-    ItemSeparator:{
-        backgroundColor:"#ddd",
-        height:1,
-        width:"100%"
+    ItemSeparator: {
+        backgroundColor: "#ddd",
+        height: 1,
+        width: "100%"
 
     },
-    floatingButton:{
-        position:"absolute",
-        bottom:20,
-        right:10
+    floatingButton: {
+        position: "absolute",
+        bottom: 20,
+        right: 10
     }
 
 });
